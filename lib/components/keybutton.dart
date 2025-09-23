ed2a32ad09f8ef6c94ed89dfc8270e37bd4e5866
marwans12 <marwans12@hotmail.com>
2025-09-23 19:30:50 +0530

    Update KeyButton Fields

--- lib/components/keybutton.dart
+++ lib/components/keybutton.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 
 class KeyButton extends StatelessWidget {
-  const KeyButton({super.key, required this.child, required this.callback});
+  const KeyButton(
+      {super.key,
+      required this.child,
+      required this.callback,
+      required this.keyValue});
   final Widget child;
-  final void Function()? callback;
-
+  final void Function(String keyValue) callback;
+  final String keyValue;
   @override
   Widget build(BuildContext context) {
     var stockButton = FilledButton(
-        onPressed: () {},
+        onPressed: () {
+                callback(keyValue);
+              },
         style: FilledButton.styleFrom(
             backgroundColor: const Color.fromARGB(255, 73, 73, 73),
             foregroundColor: Colors.white,
