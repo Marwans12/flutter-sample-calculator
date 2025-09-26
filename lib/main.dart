b3ebf140d28ed66ff731fd1aaaad64e17f380ee7
marwans12 <marwans12@hotmail.com>
2025-09-26 19:34:18 +0530

    Add Change Notifier To The Tree

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:everything_calculator/Structure/keys_area.dart';
 import 'package:everything_calculator/Structure/typing_area.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MainApp());
@@ -12,15 +13,31 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark(),
+        theme: ThemeData.dark(),
         home: Scaffold(
             body: Container(
-              color: const Color.fromARGB(255, 49, 49, 49),
-              child: const Center(
-                  child: Column(children: [
-                    Expanded(child: TypingArea()),
-                    Expanded(child: KeysArea())
-                  ])),
-            )));
+          color: const Color.fromARGB(255, 49, 49, 49),
+          child: Center(
+              child: ChangeNotifierProvider(
+            create: (context) => Calculations(),
+            child: const Column(children: [
+              Expanded(child: TypingArea()),
+              Expanded(child: KeysArea())
+            ]),
+          )),
+        )));
   }
+}
+
+class Calculations extends ChangeNotifier {
+  // int? previousResult;
+  // int? result = 0;
+  // String expression = "";
+  // void addCharacter(keyValue) {
+  //   expression += keyValue;
+  //   calculate();
+  // }
+  // int calculate() {
+    
+  // }
 }
